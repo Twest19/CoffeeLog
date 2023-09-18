675376ab6701d1109e2edf4d2dfb07b576c74a6e
Tim West <67450649+Twest19@users.noreply.github.com>
2023-09-17 19:20:11 -0500

    Started User Defaults Storage

--- CoffeeLog/CoffeeLogApp.swift
+++ CoffeeLog/CoffeeLogApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct CoffeeLogApp: App {
+    
+    @StateObject var settings = Settings()
+    
     var body: some Scene {
         WindowGroup {
             CLTabView()
+                .environmentObject(settings)
+                .preferredColorScheme(settings.isDarkMode ? .dark : .light)
         }
     }
 }
---
+++ CoffeeLog/Screens/SettingsModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsModel.swift
+//  CoffeeLog
+//
+//  Created by Tim West on 9/17/23.
+//
+
+import Foundation
